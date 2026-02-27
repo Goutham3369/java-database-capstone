2ca93ca77ff9ff21978ae4119e436168e7895d32
Goutham3369 <gouthamsai5059@gmail.com>
2026-02-27 00:54:19 -0500

    change

--- app/src/main/java/com/project/back_end/config/WebConfig.java
+++ app/src/main/java/com/project/back_end/config/WebConfig.java
@@ -2,7 +2,6 @@ package com.project.back_end.config;
 
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.lang.NonNull; 
 
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,9 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(@NonNull CorsRegistry registry) {
-        // Allow CORS for all endpoints
-        registry.addMapping("/**")
+public void addCorsMappings(CorsRegistry registry) {        
                 .allowedOrigins("*")  // Add your frontend URL here
                 .allowedMethods("GET", "POST", "PUT", "DELETE")  // Specify allowed methods
                 .allowedHeaders("*");  // You can restrict headers if needed
